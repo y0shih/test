8ce09c2635e5b73776b74e0befddca53c15e1162
Mona Lisa <trongh1337@gmail.com>
2024-11-27 20:50:18 +0700

    sda

--- test.py
+++ test.py
@@ -2,10 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
 def hello_world():
     return 'Hello, World!'
     return 'Hello, World!'
